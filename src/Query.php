b2c99cd2c007ecce456a0e402d518db47c6530a1
Matias Camiletti <matias.camiletti@gmail.com>
2022-11-29 18:38:51 -0300

    Create query file.

---
+++ src/Query.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tots\EloFilter;
+
+class Query
+{
+
+    public function execute()
+    {
+
+    }
+
+    public static function run()
+    {
+        $obj = new Query();
+        return $obj->execute();
+    }
+}
